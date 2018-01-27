81d0ff9ae7930412d76d04252f60366e23a468cf
Ushinro <Ushinro@tutanota.com>
2018-01-27 12:54:59 -0500

    Add default questions and their respective options
    By utilizing Laravel's database seeder, default data can cleanly be created for the purposes of testing.
    
    The QuestionsTableSeeder is likely not the best implementation of the seeder since it also has the side effect of inserting into the question_options table.
    This was done so that the question choices could accurately be linked to the questions they belonged to without additional queries to fetch the questions that were just inserted.

---
+++ database/seeds/QuestionsTableSeeder.php
@@ -0,0 +1,103 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Question;
+use App\QuestionOption;
+
+/**
+ * Class QuestionsTableSeeder
+ */
+class QuestionsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $question1 = factory(Question::class)->create([
+            'label' => 'What type of car do you own?',
+            'type' => 'radio',
+        ]);
+        $question2 = factory(Question::class)->create([
+            'label' => 'How many world languages do you know?',
+            'type' => 'radio',
+        ]);
+        $question3 = factory(Question::class)->create([
+            'label' => 'What genres of books do you enjoy reading?',
+            'type' => 'checkbox',
+        ]);
+
+        // Probably not ideal to also insert choices in the same seeder.
+        // However, this does save on a few DB calls,
+        // since we will have the question ID to meet the question_id foreign key constraint.
+        $question1Choice1 = factory(QuestionOption::class)->create([
+            'question_id' => $question1->id,
+            'label' => 'American',
+            'value' => 'american',
+        ]);
+        $question1Choice2 = factory(QuestionOption::class)->create([
+            'question_id' => $question1->id,
+            'label' => 'European',
+            'value' => 'european',
+        ]);
+        $question1Choice3 = factory(QuestionOption::class)->create([
+            'question_id' => $question1->id,
+            'label' => 'Japanese',
+            'value' => 'japanese',
+        ]);
+        $question1Choice4 = factory(QuestionOption::class)->create([
+            'question_id' => $question1->id,
+            'label' => 'I do not own a car',
+            'value' => 'none',
+        ]);
+
+        $question2Choice1 = factory(QuestionOption::class)->create([
+            'question_id' => $question2->id,
+            'label' => '1',
+            'value' => '1',
+        ]);
+        $question2Choice2 = factory(QuestionOption::class)->create([
+            'question_id' => $question2->id,
+            'label' => '2',
+            'value' => '2',
+        ]);
+        $question2Choice3 = factory(QuestionOption::class)->create([
+            'question_id' => $question2->id,
+            'label' => 'Greater than 2',
+            'value' => '>2',
+        ]);
+
+        $question3Choice1 = factory(QuestionOption::class)->create([
+            'question_id' => $question3->id,
+            'label' => 'Fiction',
+            'value' => 'fiction',
+        ]);
+        $question3Choice2 = factory(QuestionOption::class)->create([
+            'question_id' => $question3->id,
+            'label' => 'Non-fiction',
+            'value' => 'nonfiction',
+        ]);
+        $question3Choice3 = factory(QuestionOption::class)->create([
+            'question_id' => $question3->id,
+            'label' => 'Sci-Fi',
+            'value' => 'scifi',
+        ]);
+        $question3Choice4 = factory(QuestionOption::class)->create([
+            'question_id' => $question3->id,
+            'label' => 'Mystery',
+            'value' => 'mystery',
+        ]);
+        $question3Choice5 = factory(QuestionOption::class)->create([
+            'question_id' => $question3->id,
+            'label' => 'Romance',
+            'value' => 'romance',
+        ]);
+        $question3Choice6 = factory(QuestionOption::class)->create([
+            'question_id' => $question3->id,
+            'label' => 'I do not read books (often)',
+            'value' => 'none',
+        ]);
+    }
+}
